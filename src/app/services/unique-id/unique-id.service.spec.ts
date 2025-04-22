ef0bcfa7e8f2a38f7e29614da305f5ef9ea305ec
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-22 12:13:38 -0300

    test: incluido um teste que verifica que não há ids duplicados

--- src/app/services/unique-id/unique-id.service.spec.ts
+++ src/app/services/unique-id/unique-id.service.spec.ts
@@ -21,4 +21,16 @@ describe(UniqueIdService.name, () => {
     // expect(id).toMatch(/^app-\d+/); // Utilizando regex
     expect(id.startsWith('app-')).toBeTrue(); // Verifica se começa com 'app-'
   });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate ids when called multiple times`, () => {
+    const ids: Set<string> = new Set<string>();
+
+    for (let i = 0; i < 50; i++) {
+      const id = service.generateUniqueIdWithPrefix('app');
+
+      ids.add(id);
+    }
+
+    expect(ids.size).toBe(50); // Espero que o tamanho do conjunto seja 50, ou seja, não houve duplicatas
+  });
 });
