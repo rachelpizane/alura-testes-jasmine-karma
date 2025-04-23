978b0f36a80467faffe385240998b8e25f7f286f
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-23 11:29:17 -0300

    test: incluido teste para o componente

--- src/app/components/like-widget/like-widget.component.spec.ts
+++ src/app/components/like-widget/like-widget.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LikeWidgetComponent } from './like-widget.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-describe('LikeWidgetComponent', () => {
+describe(LikeWidgetComponent.name, () => {
   let component: LikeWidgetComponent;
   let fixture: ComponentFixture<LikeWidgetComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ]
+      declarations: [ LikeWidgetComponent ],
+      imports: [FontAwesomeModule],
     })
     .compileComponents();
   });
@@ -16,10 +18,20 @@ describe('LikeWidgetComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LikeWidgetComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.detectChanges(); // Detecta e aplica as mudanças no componente e no template.
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should auto generate id when id input property is missing', () => {
+    expect(component.id).toBeTruthy();
+  });
+
+  it('should not auto generate id when id input property is present', () => {
+    component.id = 'custom-id';
+    fixture.detectChanges(); // Atualiza o template com as mudanças feitas no componente.
+    expect(component.id).toBe('custom-id');
   });
 });
